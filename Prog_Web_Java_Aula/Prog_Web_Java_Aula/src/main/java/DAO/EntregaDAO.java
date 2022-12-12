5477eca25ea9316fe706acf8441b7d1c87993812
Villat <juninho_lima2020@hotmail.com>
2022-12-12 00:26:46 -0300

    EntregaDAO Completo

--- Prog_Web_Java_Aula/Prog_Web_Java_Aula/src/main/java/DAO/EntregaDAO.java
+++ Prog_Web_Java_Aula/Prog_Web_Java_Aula/src/main/java/DAO/EntregaDAO.java
@@ -1,27 +1,126 @@
 package DAO;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import VO.Entrega;
 
-public class EntregaDAO {
+public class EntregaDAO extends BaseDAO {
 
-	public void saveEntrega() {
-		
+	public EntregaDAO() {
+
 	}
-	
-	public void loadEntrega() {
-		
+
+	public EntregaDAO(Entrega vo) {
+		this.vo = vo;
 	}
-	
-	public List<Entrega> getCategoria() {
+
+	Entrega vo;
+
+	public Entrega getVo() {
+		return vo;
+	}
+
+	public void setVo(Entrega vo) {
+		this.vo = vo;
+	}
+
+	public void newEntrega(Entrega e) throws Exception {
+
+		abrir();
+		String query = " insert into entrega (venda) values (?)";
+		PreparedStatement preparedStmt = getConn().prepareStatement(query);
+		preparedStmt.setString(1, e.getVenda());
+		preparedStmt.execute();
+		fechar();
+
+	}
+
+	public List<Entrega> getEntrega() throws Exception {
 		List<Entrega> lst = new ArrayList<Entrega>();
+		abrir();
+		Statement s = getConn().createStatement();
+		String sql = "SELECT id, venda, data FROM entrega";
+		ResultSet rs = s.executeQuery(sql);
+		while (rs.next()) {
+			Entrega c = new Entrega();
+			c.setId(rs.getInt(1));
+			c.setVenda(rs.getString(2));
+			c.setData(rs.getInt(3));
+
+			lst.add(c);
+		}
+		s.close();
+		rs.close();
+		fechar();
 		return lst;
 	}
-	
-	public void deleteEntrega(){
-		
+
+	public void deleteEntrega() throws Exception {
+		abrir();
+		String query = " delete from entrega where id=?";
+		PreparedStatement preparedStmt = getConn().prepareStatement(query);
+		preparedStmt.setInt(1, vo.getId());
+		preparedStmt.execute();
+		fechar();
 	}
-	
+
+	public void editEntrega(Entrega e) throws Exception {
+
+		abrir();
+		String query = " update entrega set venda=?, data=? where id=?";
+		PreparedStatement preparedStmt = getConn().prepareStatement(query);
+		preparedStmt.setString(1, e.getVenda());
+		preparedStmt.setInt(3, e.getData());
+		preparedStmt.setInt(5, e.getId());
+		preparedStmt.execute();
+		fechar();
+	}
+
+	public ArrayList<Entrega> findEntrega(String venda) throws Exception {
+
+		super.abrir();
+		ArrayList<Entrega> lst = new ArrayList();
+		String sql = "SELECT id,venda, data, FROM entrega where venda like ?";
+		PreparedStatement preparedStmt = getConn().prepareStatement(sql);
+		preparedStmt.setString(1, venda + '%');
+		ResultSet rs = preparedStmt.executeQuery();
+		while (rs.next()) {
+			Entrega e = new Entrega();
+			e.setId(Integer.parseInt(rs.getString(1)));
+			e.setVenda(rs.getString(2));
+			e.setData(rs.getInt(3));
+			lst.add(e);
+		}
+		preparedStmt.close();
+		rs.close();
+		fechar();
+		return lst;
+	}
+
+	public Entrega findByIdEntrega(int id) throws Exception {
+
+		super.abrir();
+
+		String sql = "SELECT id, venda, data FROM entrega where id=?";
+
+		PreparedStatement preparedStmt = getConn().prepareStatement(sql);
+
+		preparedStmt.setInt(1, id);
+
+		ResultSet rs = preparedStmt.executeQuery();
+		Entrega e = new Entrega();
+		while (rs.next()) {
+			e.setId(Integer.parseInt(rs.getString(1)));
+			e.setVenda(rs.getString(2));
+			e.setData(rs.getInt(3));
+		}
+
+		rs.close();
+		return e;
+	}
+
 }
